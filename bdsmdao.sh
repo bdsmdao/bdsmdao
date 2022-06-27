cd8823772f7c41996d26de3651f9bbd71cafacae
bdsmdao <108298463+bdsmdao@users.noreply.github.com>
2022-06-27 15:00:19 -0400

    Create bdsmdao.sh

---
+++ bdsmdao.sh
@@ -0,0 +1,4 @@
+#!bin bash
+echo "WARNING: The BDSM Distributed Autonomous Organization (DAO) contains adult contact not suitable for minors or those not interested in BDSM content."
+echo
+echo "Do you understand the nature of this program and wish to continue? (Y/N)
