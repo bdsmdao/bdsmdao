42fed9d2e8a043b5fbc993af60ec30c4c044f410
bdsmdao <108298463+bdsmdao@users.noreply.github.com>
2022-06-27 15:12:09 -0400

    Update bdsmdao.sh

--- bdsmdao.sh
+++ bdsmdao.sh
@@ -1,11 +1,11 @@
 #!bin bash
 echo "WARNING: The BDSM Distributed Autonomous Organization (DAO) contains adult contact not suitable for minors or those not interested in BDSM content."
 echo
-echo "Do you understand the nature of this program and consent to continue? (y/n)
+read 'Do you understand the nature of this program and consent to continue? (y/n) ' consent
 read consent
-if consent="y" then
-  echo "Thank you for consenting to join the BDSM DAO."
+if consent='y' then
+  echo 'Thank you for consenting to join the BDSM DAO.'
 else
-  echo "You have safeworded. The DAO will shutdown now that you have safeworded."
+  echo 'You have safeworded. The DAO will shutdown now.'
   exit
-echo "Welcome to the BDSM DAO."
+echo 'Welcome to the BDSM DAO.'
